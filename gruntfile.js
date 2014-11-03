67505ca4ace7e6daec1f4fde041327cfae1d201c
Nick Phillips <nick@linkstudios.co.uk>
2014-11-03 21:44:11 +0000

    Start of using grunt to manage less/js files #19

---
+++ gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    jshint: {
+      all: ['gruntfile.js']
+    },
+
+    less: {
+      files: {
+        src: 'bower_components/bootstrap/less/bootstrap.less',
+        dest: 'web/assets/css/style.css'
+      }
+    },
+
+    uglify: {
+      scripts: {
+        files: {
+          'web/assets/js/main.js': [
+            'bower_components/angular/angular.min.js',
+            'bower_components/angular-bootstrap/ui-bootstrap.min.js'
+          ]
+        }
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [],
+        tasks: ['uglify']
+      },
+      styles: {
+        files: ['**/*.less'],
+        tasks: ['less']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'less', 'uglify']);
+  grunt.registerTask('test', ['jshint']);
+};
