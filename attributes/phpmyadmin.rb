ed47159c4160e98a20290d8efd6236d43d403bfc
Nick Phillips <nick@linkstudios.co.uk>
2014-11-08 00:09:12 +0000

    PHPmyadmin notes #8

--- attributes/phpmyadmin.rb
+++ attributes/phpmyadmin.rb
@@ -1,4 +1,6 @@
 default['phpmyadmin']['user']          = 'phpmyadmin'
 default['phpmyadmin']['group']         = 'vagrant'
+# Although we use PHP-FPM, the cookbook uses php_fpm which isn't supported in our chosen PHP-FPM cookbook
+# so we manage this manually with a template
 default['phpmyadmin']['fpm']           = false
 default['phpmyadmin']['port']          = 1090
