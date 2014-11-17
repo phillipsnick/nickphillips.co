4f9f41f8b1de22a8c5291bb8874ef0a1e04bba37
Nick Phillips <nick@linkstudios.co.uk>
2014-11-17 16:52:49 +0000

    Attempting to get AngularJS working #60

--- src/App/DefaultBundle/Controller/DefaultController.php
+++ src/App/DefaultBundle/Controller/DefaultController.php
@@ -8,6 +8,10 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $this->get('session')->getFlashBag()->add(
+            'success',
+            'This is a success message!'
+        );
         $name = 'WOW';
         return $this->render('AppDefaultBundle:Default:index.html.twig', array('name' => $name));
     }
